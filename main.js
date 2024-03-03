0ae9c23a77ca150aeba62518807e928151518886
Angolapa <josealfredoangola@gmail.com>
2024-03-03 00:34:53 -0300

    Arreglo Js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-function toggleParagraph() {
-    var paragraph = document.getElementById("paragraph");
-    var icon = document.getElementById("icon");
-  
-    if (paragraph.style.display === "none" || !paragraph.style.display) {
-      paragraph.style.display = "block";
-      icon.src = "./assets/images/icon-minus.svg";
-    } else {
-      paragraph.style.display = "none";
-      icon.src = "./assets/images/icon-plus.svg";
-    }
+function toggleParagraph(containerId) {
+  var container = document.getElementById(containerId);
+  var paragraph = container.querySelector(".main-container__s__details__div__p");
+  var icon = container.querySelector(".main-container__s2__div__icon");
+
+  if (paragraph.style.display === "none" || !paragraph.style.display) {
+    paragraph.style.display = "block";
+    icon.src = "./assets/images/icon-minus.svg";
+  } else {
+    paragraph.style.display = "none";
+    icon.src = "./assets/images/icon-plus.svg";
   }
+}
