dc88062fa228317e38d2e969f8d8ae27809ca8f4
Angolapa <josealfredoangola@gmail.com>
2024-03-03 11:23:43 -0300

    Terminada la parte mobile

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function toggleParagraph(containerId) {
   var container = document.getElementById(containerId);
-  var paragraph = container.querySelector(".main-container__s__details__div__p");
+  var paragraph = container.querySelector(".main-container__s__div__p");
   var icon = container.querySelector(".main-container__s2__div__icon");
 
   if (paragraph.style.display === "none" || !paragraph.style.display) {
     paragraph.style.display = "block";
-    icon.src = "./assets/images/icon-minus.svg"; // Reemplaza con la ruta correcta
+    icon.src = "./assets/images/icon-minus.svg";
   } else {
     paragraph.style.display = "none";
-    icon.src = "./assets/images/icon-plus.svg"; // Reemplaza con la ruta correcta
+    icon.src = "./assets/images/icon-plus.svg"; 
   }
 }
